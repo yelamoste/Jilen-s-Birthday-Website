94e0edd775459bea19a136045f154aadd705c80a
yelamoste <108941678+yelamoste@users.noreply.github.com>
2024-11-01 03:02:58 +0800

    sdfhsdj

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Jilen Maica Barbosa</title>
+    <link rel="stylesheet" href="index.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&family=Quicksand:wght@300..700&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Fredoka:wght@300..700&family=Quicksand:wght@300..700&display=swap');
+    </style>
+</head>
+<body>
+<?php
+
+echo " <div class='main-container'>
+    <div class='side-bar-cont'>Side bar</div>
+    <div class='homepage-cont'>
+        <div class='birthday-cont'>Happy Birthday,</div>
+        <div class='celebrant-name-cont'>Jilen Maica Barbosa!</div>
+        <div class='celebrant-picture-cont'>
+            <img src=''/>
+        </div>
+    </div>
+</div>";
+?>
+</body>
+</html>
